75ca7467334ba4183b9aa66c426ee814cc4af9a7
Lee Hyunkyung <michelle3hg@gmail.com>
2026-05-18 15:12:13 +0900

    feat: 주문관련 schema.sql 작성 완료

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,6 +1,49 @@
-DROP TABLE IF EXISTS member_conn_test;
+-- 주문상태 테이블
+CREATE TABLE order_status
+(
+    status_id   BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(50) NOT NULL UNIQUE
+);
 
-CREATE TABLE member_conn_test (
-    member_id INT AUTO_INCREMENT PRIMARY KEY,
-    nickname VARCHAR(50) NOT NULL
+
+-- 주문 테이블
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders
+(
+    order_id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id        BIGINT       NOT NULL,
+    order_date       DATETIME(6) NOT NULL,
+    shipping_address VARCHAR(255) NOT NULL,
+    total_price      BIGINT       NOT NULL,
+
+    FOREIGN KEY (member_id)
+        REFERENCES member (member_id)
+        ON DELETE RESTRICT
+);
+
+CREATE INDEX idx_orders_order_date ON orders (order_date);
+CREATE INDEX idx_orders_member_id_order_date ON orders (member_id, order_date);
+
+-- 주문목록 테이블
+DROP TABLE IF EXISTS order_item;
+
+CREATE TABLE order_item
+(
+    order_item_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id      BIGINT NOT NULL,
+    product_id    BIGINT NOT NULL,
+    status_id     BIGINT NOT NULL,
+    quantity      BIGINT NOT NULL DEFAULT 1,
+    order_price   BIGINT NOT NULL,
+
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON DELETE RESTRICT,
+    FOREIGN KEY (status_id)
+        REFERENCES order_status (status_id)
+        ON DELETE RESTRICT
 );
