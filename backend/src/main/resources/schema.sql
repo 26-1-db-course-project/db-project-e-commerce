b472bccb9e53dd30bf4331dc2f8c974084d9a56b
coderin3 <hyerinpark@ewha.ac.kr>
2026-05-27 10:18:10 +0900

    Update schema.sql

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -48,15 +48,6 @@ CREATE TABLE member
     grade_id    BIGINT  NOT NULL,
     total_purchase_amount DECIMAL(15,2) NOT NULL DEFAULT 0,
     report_count          INT           NOT NULL DEFAULT 0,
-    member_id             BIGINT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    login_id              VARCHAR(50)    NOT NULL UNIQUE, -- 기존의 문자열 ID 역할을 할 로그인용 아이디
-    password              VARCHAR(255)   NOT NULL,
-    email                 VARCHAR(100)   NOT NULL,
-    phone_number          VARCHAR(20)    NOT NULL,
-    status_name           VARCHAR(20)    NOT NULL,
-    grade_name            VARCHAR(20)    NOT NULL,
-    total_purchase_amount DECIMAL(15, 2) NOT NULL DEFAULT 0,
-    report_count          INT            NOT NULL DEFAULT 0,
     FOREIGN KEY (status_name) REFERENCES activity_status (status_name)
         ON UPDATE CASCADE
         ON DELETE RESTRICT,
